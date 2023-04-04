2b17dcaab2efbc5bd6b6d24727ccc33ced5c84af
Manuel <manuelwiltz@gmail.com>
2023-04-04 15:11:51 +0200

    first commit

---
+++ qr/src/main/java/qr/code/qr/Config.java
@@ -0,0 +1,18 @@
+package qr.code.qr;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.BufferedImageHttpMessageConverter;
+import org.springframework.http.converter.HttpMessageConverter;
+
+import java.awt.image.BufferedImage;
+
+@Configuration
+public class Config {
+
+    @Bean
+    public HttpMessageConverter<BufferedImage> createImageHttpMessageConverter() {
+        return new BufferedImageHttpMessageConverter();
+    }
+
+}
---
+++ qr/src/main/java/qr/code/qr/RestController.java
@@ -0,0 +1,19 @@
+package qr.code.qr;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.awt.image.BufferedImage;
+
+@Controller
+@RequestMapping("/qrcode")
+public class RestController {
+
+    @PostMapping(value = "/generate", produces = MediaType.IMAGE_PNG_VALUE)
+    public ResponseEntity<BufferedImage> getQRCode(@RequestBody String link) throws Exception {
+        return ResponseEntity.ok(QRCodeGenerator.generateQRCodeImage(link));
+    }
+
+}
